1df0427914fc787895f203f82550e60f21ed7814
Yesid Dev <yesid@dsd.dev>
2026-02-22 11:58:10 -0500

    Matter Over Thread (Sensor- ESP32 H2)

--- README.md
+++ README.md
@@ -1,56 +1,82 @@
-# Thread Border Router
+# Matter Sensors
 
-This example creates a Matter Thread Border Router device using the ESP Matter data model.
+This example demonstrates the integration of temperature and humidity sensors (SHTC3)
+and an occupancy sensor (PIR). 
 
+This application creates the temperature sensor, humidity sensor, and occupancy sensor
+on endpoint 1, 2, and 3 respectively.
 
-See the [docs](https://docs.espressif.com/projects/esp-matter/en/latest/esp32/developing.html) for more information about building and flashing the firmware.
+See the [docs](https://docs.espressif.com/projects/esp-matter/en/latest/esp32/developing.html)
+for more information about building and flashing the firmware.
 
-## 1. Additional Environment Setup
+## Connecting the sensors
 
-### 1.1 Hardware Platform
+- Connecting the SHTC3, temperature and humidity sensor
 
-The [ESP Thread Border Router board](https://github.com/espressif/esp-thread-br?tab=readme-ov-file#esp-thread-border-router-board) which provides an integrated module of an ESP32-S3 and an ESP32-H2 is required for this example.
+| ESP32-C3 Pin | SHTC3 Pin |
+|--------------|-----------|
+| GND          | GND       |
+| 3V3          | VCC       |
+| GPIO 4       | SDA       |
+| GPIO 5       | SCL       |
 
-### 1.2 Firmware for RCP
+- Connecting the PIR sensor
 
-The [OpenThread RCP](https://github.com/espressif/esp-idf/tree/master/examples/openthread/ot_rcp) should be run on ESP32-H2 of the Border Router board. You can flash it directly:
+| ESP32-C3 Pin | PIR Pin |
+|--------------|---------|
+| GND          | GND     |
+| 3V3          | VCC     |
+| GPIO 7       | Output  |
 
+**_NOTE:_**:
+- Above mentioned wiring connection is configured by default in the example.
+- Ensure that the GPIO pins used for the sensors are correctly configured through menuconfig.
+- Modify the configuration parameters as needed for your specific hardware setup.
 
+## Usage
+
+- Commission the app using Matter controller and read the attributes.
+
+Below, we are using chip-tool to commission and subscribe the sensor attributes.
+- 
 ```
-$ cd /path/to/esp-idf/examples/openthread/ot_rcp
-$ idf.py set-target esp32h2 build
-$ idf.py -p <port> erase-flash flash
+# Commission
+chip-tool pairing ble-wifi 1 (SSID) (PASSPHRASE) 20202021 3840
+
+# Start chip-tool in interactive mode
+chip-tool interactive start
+
+# Subscribe to attributes
+> temperaturemeasurement subscribe measured-value 3 10 1 1
+> relativehumiditymeasurement subscribe measured-value 3 10 1 2
+> occupancysensing subscribe occupancy 3 10 1 3
 ```
 
-Or you can flash the firmware of ESP32-H2 with [esp_rcp_update](https://github.com/espressif/esp-thread-br/tree/main/components/esp_rcp_update) after enabling `AUTO_UPDATE_RCP` in menuconfig:
+## 🛠️ Troubleshooting
 
-```
-$ cd /path/to/esp-idf/examples/openthread/ot_rcp
-$ idf.py set-target esp32h2 build
-```
+If you encounter the following runtime error:
 
-After flashing the Thread Border Router firmware to ESP32-S3, it will flash the RCP firmware to ESP32-H2 automatically.
+`
+i2c: CONFLICT! driver_ng is not allowed to be used with this old driver
+`
 
-### 1.3 Firmware for Host SoC
+This error occurs due to a conflict between the legacy I2C driver and the newer driver model (`driver_ng`).
 
-The default setting flash size is 8MB, set target and build as below:
+### ✅ Solution
 
-```
-$ idf.py set-target esp32s3
-$ idf.py build
-```
+Enable the following option via `idf.py menuconfig`:
 
-## 2. Post Commissioning Setup
+`CONFIG_I2C_SKIP_LEGACY_CONFLICT_CHECK=y`
 
-After commissioning the Border Router with chip-tool, you can set up the Thread network with ThreadBorderRouterManagement cluster.
 
-```
-$ ./chip-tool generalcommissioning arm-fail-safe 180 1 0x7283 0
-$ ./chip-tool threadborderroutermanagement set-active-dataset-request hex:<thread-dataset-tlvs> 0x7283 1
-$ ./chip-tool generalcommissioning commissioning-complete 0x7283 0
-```
-Then the Thread Border Router will form/join a Thread network and you can commission a Thread End-device to the Thread network with chip-tool.
+**Important**: This option is only available in the **latest ESP-IDF release branches**:
 
-```
-$ ./chip-tool pairing ble-thread 0x7384 hex:<thread-dataset-tlvs> 20202021 3840
-```
+Pull the latest code from release branches.
+
+- `release/v5.2`
+- `release/v5.3`
+- `release/v5.4`
+- `release/v5.5`
+- `master`
+
+- If you're using an older ESP-IDF version, you can apply this [commit as a patch](https://github.com/espressif/esp-idf/commit/466328cd7e4c90c749a406d2bcee73f782ac0016) to add support manually.
--- main/app_main.cpp
+++ main/app_main.cpp
@@ -6,25 +6,22 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 
+#include <app/server/CommissioningWindowManager.h>
+#include <app/server/Server.h>
+#include <bsp/esp-bsp.h>
 #include <esp_err.h>
 #include <esp_log.h>
-#include <nvs_flash.h>
-#include <esp_openthread_lock.h>
-#include <esp_openthread_border_router.h>
-#include <esp_spiffs.h>
-
 #include <esp_matter.h>
-#include <esp_matter_console.h>
-#include <esp_matter_feature.h>
+#include <esp_matter_ota.h>
+#include <nvs_flash.h>
 
+#include <app_openthread_config.h>
 #include <app_reset.h>
-#include <esp_ot_config.h>
+#include <common_macros.h>
 
-#include <platform/ESP32/OpenthreadLauncher.h>
-#include <platform/OpenThread/GenericThreadBorderRouterDelegate.h>
-#include <app/server/Server.h>
-#include <credentials/FabricTable.h>
-#include <platform/KvsPersistentStorageDelegate.h>
+// drivers implemented by this example
+#include <drivers/shtc3.h>
+#include <drivers/pir.h>
 
 static const char *TAG = "app_main";
 
@@ -32,75 +29,187 @@ using namespace esp_matter;
 using namespace esp_matter::attribute;
 using namespace esp_matter::endpoint;
 using namespace chip::app::Clusters;
-using chip::app::Clusters::ThreadBorderRouterManagement::GenericOpenThreadBorderRouterDelegate;
+
+// Application cluster specification, 7.18.2.11. Temperature
+// represents a temperature on the Celsius scale with a resolution of 0.01°C.
+// temp = (temperature in °C) x 100
+static void temp_sensor_notification(uint16_t endpoint_id, float temp, void *user_data)
+{
+    // schedule the attribute update so that we can report it from matter thread
+    chip::DeviceLayer::SystemLayer().ScheduleLambda([endpoint_id, temp]() {
+        attribute_t * attribute = attribute::get(endpoint_id,
+                                                 TemperatureMeasurement::Id,
+                                                 TemperatureMeasurement::Attributes::MeasuredValue::Id);
+
+        esp_matter_attr_val_t val = esp_matter_invalid(NULL);
+        attribute::get_val(attribute, &val);
+        val.val.i16 = static_cast<int16_t>(temp * 100);
+
+        attribute::update(endpoint_id, TemperatureMeasurement::Id, TemperatureMeasurement::Attributes::MeasuredValue::Id, &val);
+    });
+}
+
+// Application cluster specification, 2.6.4.1. MeasuredValue Attribute
+// represents the humidity in percent.
+// humidity = (humidity in %) x 100
+static void humidity_sensor_notification(uint16_t endpoint_id, float humidity, void *user_data)
+{
+    // schedule the attribute update so that we can report it from matter thread
+    chip::DeviceLayer::SystemLayer().ScheduleLambda([endpoint_id, humidity]() {
+        attribute_t * attribute = attribute::get(endpoint_id,
+                                                 RelativeHumidityMeasurement::Id,
+                                                 RelativeHumidityMeasurement::Attributes::MeasuredValue::Id);
+
+        esp_matter_attr_val_t val = esp_matter_invalid(NULL);
+        attribute::get_val(attribute, &val);
+        val.val.u16 = static_cast<uint16_t>(humidity * 100);
+
+        attribute::update(endpoint_id, RelativeHumidityMeasurement::Id, RelativeHumidityMeasurement::Attributes::MeasuredValue::Id, &val);
+    });
+}
+
+static void occupancy_sensor_notification(uint16_t endpoint_id, bool occupancy, void *user_data)
+{
+    // schedule the attribute update so that we can report it from matter thread
+    chip::DeviceLayer::SystemLayer().ScheduleLambda([endpoint_id, occupancy]() {
+        attribute_t * attribute = attribute::get(endpoint_id,
+                                                 OccupancySensing::Id,
+                                                 OccupancySensing::Attributes::Occupancy::Id);
+
+        esp_matter_attr_val_t val = esp_matter_invalid(NULL);
+        attribute::get_val(attribute, &val);
+        val.val.b = occupancy;
+
+        attribute::update(endpoint_id, OccupancySensing::Id, OccupancySensing::Attributes::Occupancy::Id, &val);
+    });
+}
+
+static esp_err_t factory_reset_button_register()
+{
+    button_handle_t push_button;
+    esp_err_t err = bsp_iot_button_create(&push_button, NULL, BSP_BUTTON_NUM);
+    VerifyOrReturnError(err == ESP_OK, err);
+    return app_reset_button_register(push_button);
+}
+
+static void open_commissioning_window_if_necessary()
+{
+    VerifyOrReturn(chip::Server::GetInstance().GetFabricTable().FabricCount() == 0);
+
+    chip::CommissioningWindowManager  &commissionMgr = chip::Server::GetInstance().GetCommissioningWindowManager();
+    VerifyOrReturn(commissionMgr.IsCommissioningWindowOpen() == false);
+
+    // After removing last fabric, this example does not remove the Wi-Fi credentials
+    // and still has IP connectivity so, only advertising on DNS-SD.
+    CHIP_ERROR err = commissionMgr.OpenBasicCommissioningWindow(chip::System::Clock::Seconds16(300),
+                                                                chip::CommissioningWindowAdvertisement::kDnssdOnly);
+    if (err != CHIP_NO_ERROR) {
+        ESP_LOGE(TAG, "Failed to open commissioning window, err:%" CHIP_ERROR_FORMAT, err.Format());
+    }
+}
 
 static void app_event_cb(const ChipDeviceEvent *event, intptr_t arg)
 {
     switch (event->Type) {
-    case chip::DeviceLayer::DeviceEventType::PublicEventTypes::kInterfaceIpAddressChanged:
-        ESP_LOGI(TAG, "Interface IP Address changed");
+    case chip::DeviceLayer::DeviceEventType::kCommissioningComplete:
+        ESP_LOGI(TAG, "Commissioning complete");
         break;
-    case chip::DeviceLayer::DeviceEventType::kESPSystemEvent:
-        if (event->Platform.ESPSystemEvent.Base == IP_EVENT &&
-                event->Platform.ESPSystemEvent.Id == IP_EVENT_STA_GOT_IP) {
-#if CONFIG_OPENTHREAD_BORDER_ROUTER
-            static bool sThreadBRInitialized = false;
-            if (!sThreadBRInitialized) {
-                esp_openthread_set_backbone_netif(esp_netif_get_handle_from_ifkey("WIFI_STA_DEF"));
-                esp_openthread_lock_acquire(portMAX_DELAY);
-                esp_openthread_border_router_init();
-                esp_openthread_lock_release();
-                sThreadBRInitialized = true;
-            }
-#endif
-        }
+
+    case chip::DeviceLayer::DeviceEventType::kFailSafeTimerExpired:
+        ESP_LOGI(TAG, "Commissioning failed, fail safe timer expired");
         break;
+
+    case chip::DeviceLayer::DeviceEventType::kFabricRemoved:
+        ESP_LOGI(TAG, "Fabric removed successfully");
+        open_commissioning_window_if_necessary();
+        break;
+
+    case chip::DeviceLayer::DeviceEventType::kBLEDeinitialized:
+        ESP_LOGI(TAG, "BLE deinitialized and memory reclaimed");
+        break;
+
     default:
         break;
     }
 }
 
+// This callback is invoked when clients interact with the Identify Cluster.
+// In the callback implementation, an endpoint can identify itself. (e.g., by flashing an LED or light).
+static esp_err_t app_identification_cb(identification::callback_type_t type, uint16_t endpoint_id, uint8_t effect_id,
+                                       uint8_t effect_variant, void *priv_data)
+{
+    ESP_LOGI(TAG, "Identification callback: type: %u, effect: %u, variant: %u", type, effect_id, effect_variant);
+    return ESP_OK;
+}
+
+// This callback is called for every attribute update. The callback implementation shall
+// handle the desired attributes and return an appropriate error code. If the attribute
+// is not of your interest, please do not return an error code and strictly return ESP_OK.
+static esp_err_t app_attribute_update_cb(attribute::callback_type_t type, uint16_t endpoint_id, uint32_t cluster_id,
+                                         uint32_t attribute_id, esp_matter_attr_val_t *val, void *priv_data)
+{
+    // Since this is just a sensor and we don't expect any writes on our temperature sensor,
+    // so, return success.
+    return ESP_OK;
+}
+
 extern "C" void app_main()
 {
-    esp_err_t err = ESP_OK;
-
     /* Initialize the ESP NVS layer */
     nvs_flash_init();
-    // If there is no commissioner in the controller, we need a default node so that the controller can be commissioned
-    // to a specific fabric.
+
+    /* Initialize push button on the dev-kit to reset the device */
+    esp_err_t err = factory_reset_button_register();
+    ABORT_APP_ON_FAILURE(ESP_OK == err, ESP_LOGE(TAG, "Failed to initialize reset button, err:%d", err));
+
+    /* Create a Matter node and add the mandatory Root Node device type on endpoint 0 */
     node::config_t node_config;
-    node_t *node = node::create(&node_config, NULL, NULL);
-    static chip::KvsPersistentStorageDelegate tbr_storage_delegate;
-    chip::DeviceLayer::PersistedStorage::KeyValueStoreManager  &kvsManager = chip::DeviceLayer::PersistedStorage::KeyValueStoreMgr();
-    tbr_storage_delegate.Init(&kvsManager);
-    GenericOpenThreadBorderRouterDelegate *delegate = chip::Platform::New<GenericOpenThreadBorderRouterDelegate>(&tbr_storage_delegate);
-    char threadBRName[] = "Espressif-ThreadBR";
-    delegate->SetThreadBorderRouterName(chip::CharSpan(threadBRName));
-    if (!delegate) {
-        ESP_LOGE(TAG, "Failed to create thread_border_router delegate");
-        return;
-    }
-    thread_border_router::config_t tbr_config;
-    tbr_config.thread_border_router_management.delegate = delegate;
-    endpoint_t *tbr_endpoint = thread_border_router::create(node, &tbr_config, ENDPOINT_FLAG_NONE, NULL);
-    if (!node || !tbr_endpoint) {
-        ESP_LOGE(TAG, "Failed to create data model");
-        return;
-    }
-    cluster_t *tbr_cluster = cluster::get(tbr_endpoint, ThreadBorderRouterManagement::Id);
-    cluster::thread_border_router_management::feature::pan_change::add(tbr_cluster);
-#if CHIP_DEVICE_CONFIG_ENABLE_THREAD
-#if defined(CONFIG_OPENTHREAD_BORDER_ROUTER) && defined(CONFIG_AUTO_UPDATE_RCP)
-    esp_vfs_spiffs_conf_t rcp_fw_conf = {
-        .base_path = "/rcp_fw", .partition_label = "rcp_fw", .max_files = 10, .format_if_mount_failed = false
+    node_t *node = node::create(&node_config, app_attribute_update_cb, app_identification_cb);
+    ABORT_APP_ON_FAILURE(node != nullptr, ESP_LOGE(TAG, "Failed to create Matter node"));
+
+    // add temperature sensor device
+    temperature_sensor::config_t temp_sensor_config;
+    endpoint_t * temp_sensor_ep = temperature_sensor::create(node, &temp_sensor_config, ENDPOINT_FLAG_NONE, NULL);
+    ABORT_APP_ON_FAILURE(temp_sensor_ep != nullptr, ESP_LOGE(TAG, "Failed to create temperature_sensor endpoint"));
+
+    // add the humidity sensor device
+    humidity_sensor::config_t humidity_sensor_config;
+    endpoint_t * humidity_sensor_ep = humidity_sensor::create(node, &humidity_sensor_config, ENDPOINT_FLAG_NONE, NULL);
+    ABORT_APP_ON_FAILURE(humidity_sensor_ep != nullptr, ESP_LOGE(TAG, "Failed to create humidity_sensor endpoint"));
+
+    // initialize temperature and humidity sensor driver (shtc3)
+    static shtc3_sensor_config_t shtc3_config = {
+        .temperature = {
+            .cb = temp_sensor_notification,
+            .endpoint_id = endpoint::get_id(temp_sensor_ep),
+        },
+        .humidity = {
+            .cb = humidity_sensor_notification,
+            .endpoint_id = endpoint::get_id(humidity_sensor_ep),
+        },
     };
-    if (ESP_OK != esp_vfs_spiffs_register(&rcp_fw_conf)) {
-        ESP_LOGE(TAG, "Failed to mount rcp firmware storage");
-        return;
-    }
-    esp_rcp_update_config_t rcp_update_config = ESP_OPENTHREAD_RCP_UPDATE_CONFIG();
-    openthread_init_br_rcp(&rcp_update_config);
-#endif // CONFIG_OPENTHREAD_BORDER_ROUTER && CONFIG_AUTO_UPDATE_RCP
+    err = shtc3_sensor_init(&shtc3_config);
+    ABORT_APP_ON_FAILURE(err == ESP_OK, ESP_LOGE(TAG, "Failed to initialize temperature sensor driver"));
+
+    // add the occupancy sensor
+    occupancy_sensor::config_t occupancy_sensor_config;
+    occupancy_sensor_config.occupancy_sensing.occupancy_sensor_type =
+        chip::to_underlying(OccupancySensing::OccupancySensorTypeEnum::kPir);
+    occupancy_sensor_config.occupancy_sensing.occupancy_sensor_type_bitmap =
+        chip::to_underlying(OccupancySensing::OccupancySensorTypeBitmap::kPir);
+
+    endpoint_t * occupancy_sensor_ep = occupancy_sensor::create(node, &occupancy_sensor_config, ENDPOINT_FLAG_NONE, NULL);
+    ABORT_APP_ON_FAILURE(occupancy_sensor_ep != nullptr, ESP_LOGE(TAG, "Failed to create occupancy_sensor endpoint"));
+
+    // initialize occupancy sensor driver (pir)
+    static pir_sensor_config_t pir_config = {
+        .cb = occupancy_sensor_notification,
+        .endpoint_id = endpoint::get_id(occupancy_sensor_ep),
+    };
+    err = pir_sensor_init(&pir_config);
+    ABORT_APP_ON_FAILURE(err == ESP_OK, ESP_LOGE(TAG, "Failed to initialize occupancy sensor driver"));
+
+#if CHIP_DEVICE_CONFIG_ENABLE_THREAD
     /* Set OpenThread platform config */
     esp_openthread_platform_config_t config = {
         .radio_config = ESP_OPENTHREAD_DEFAULT_RADIO_CONFIG(),
@@ -108,17 +217,9 @@ extern "C" void app_main()
         .port_config = ESP_OPENTHREAD_DEFAULT_PORT_CONFIG(),
     };
     set_openthread_platform_config(&config);
-#endif // CHIP_DEVICE_CONFIG_ENABLE_THREAD
+#endif
 
     /* Matter start */
     err = esp_matter::start(app_event_cb);
-    if (err != ESP_OK) {
-        ESP_LOGE(TAG, "Matter start failed: %d", err);
-    }
-#if CONFIG_ENABLE_CHIP_SHELL
-    esp_matter::console::diagnostics_register_commands();
-    esp_matter::console::wifi_register_commands();
-    esp_matter::console::factoryreset_register_commands();
-    esp_matter::console::init();
-#endif // CONFIG_ENABLE_CHIP_SHELL
+    ABORT_APP_ON_FAILURE(err == ESP_OK, ESP_LOGE(TAG, "Failed to start Matter, err:%d", err));
 }
